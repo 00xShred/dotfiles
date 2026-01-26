34756628b3ea080868d505fb8177f2ce843230a7
Gabriel Windlin <gawindlin@gmail.com>
2026-01-26 07:59:56 +0100

    plugins

--- nvim/.config/nvim/lua/plugins/java.lua
+++ nvim/.config/nvim/lua/plugins/java.lua
@@ -1,4 +1,27 @@
 return {
-  -- includes: nvim-jdtls, nvim-dap (debugging), and testing support
   { import = "lazyvim.plugins.extras.lang.java" },
+
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.formatters_by_ft = opts.formatters_by_ft or {}
+      opts.formatters_by_ft.java = {}
+    end,
+  },
+
+  {
+    "mfussenegger/nvim-jdtls",
+    opts = function(_, opts)
+      opts.settings = opts.settings or {}
+      opts.settings.java = opts.settings.java or {}
+      opts.settings.java.format = opts.settings.java.format or {}
+
+      opts.settings.java.format.enabled = true
+      opts.settings.java.format.settings = {
+        url = nil,
+        profile = nil,
+      }
+    end,
+  },
 }
