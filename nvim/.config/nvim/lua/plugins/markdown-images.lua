7692f9a33b3d9167b4bb807e957496b81c5102b8
Gabriel Windlin <gawindlin@gmail.com>
2026-09-16 08:49:12 +0200

    add de/en spellcheck and auto-resize markdown images

--- nvim/.config/nvim/lua/plugins/markdown-images.lua
+++ nvim/.config/nvim/lua/plugins/markdown-images.lua
@@ -7,6 +7,7 @@ return {
         dir_path = "assets",
         relative_to_current_file = true,
         use_absolute_path = false,
+        process_cmd = "magick - -resize 700x -",
       },
       filetypes = {
         markdown = {
